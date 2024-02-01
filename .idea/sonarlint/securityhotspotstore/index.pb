064db22d384f3329f2b2adde438134b24483f63e
gustavodiniz <gustavodinniz.dev@gmail.com>
2024-02-01 08:32:08 -0300

    feat: adding fundamentals first

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+K
+fundamentals/first/first.go,9/c/9cdbac726d30281307e1982a181c19889b1df87a
---
+++ fundamentals/first/first.go
@@ -0,0 +1,13 @@
+// 1.0.0 First Program
+package main
+
+/*	Codes in Go are organized into packages
+and for uses its necessary to import them.
+*/
+import "fmt"
+
+// The port of entry of the program is the main function
+func main() {
+	fmt.Print("First")
+	fmt.Print("Program")
+}
